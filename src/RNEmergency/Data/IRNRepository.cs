26ef31dc8ea2ffd36fe40790236c2fd347dcd3c2
Simon Kang <simon.kang@gmail.com>
2013-04-22 18:15:06 +0900

    db exception handling

--- src/RNEmergency/Data/IRNRepository.cs
+++ src/RNEmergency/Data/IRNRepository.cs
@@ -23,16 +23,20 @@ namespace RNEmergency.Data
 
         public static string GetConnStr()
         {
-            var uriString = ConfigurationManager.AppSettings["ELEPHANTSQL_URL"] ??
-                ConfigurationManager.AppSettings["LOCAL_URL"];
-            var uri = new Uri(uriString);
-            var db = uri.AbsolutePath.Trim('/');
-            var user = uri.UserInfo.Split(':')[0];
-            var passwd = uri.UserInfo.Split(':')[1];
-            var port = uri.Port > 0 ? uri.Port : 5432;
-            var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
-                uri.Host, db, user, passwd, port);
-            return connStr;
+            try
+            {
+                var uriString = ConfigurationManager.AppSettings["ELEPHANTSQL_URL"] ??
+                    ConfigurationManager.AppSettings["LOCAL_URL"];
+                var uri = new Uri(uriString);
+                var db = uri.AbsolutePath.Trim('/');
+                var user = uri.UserInfo.Split(':')[0];
+                var passwd = uri.UserInfo.Split(':')[1];
+                var port = uri.Port > 0 ? uri.Port : 5432;
+                var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
+                    uri.Host, db, user, passwd, port);
+                return connStr;
+            }
+            catch { return ""; }
         }
 
         public bool VerifyDatabase()
