7fcdfa9b23c7a96c7025042d36c471e9b4f18c8c
Simon Kang <simon.kang@gmail.com>
2013-04-22 11:50:31 +0900

    Initial Web Site

---
+++ src/RNEmergency/Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace RNEmergency.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello World";
+        }
+    }
+}
