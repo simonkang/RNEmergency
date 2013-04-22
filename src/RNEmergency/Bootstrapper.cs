45c63c0feb3c23f221db7df1f7ff697c07a1062c
Simon Kang <simon.kang@gmail.com>
2013-04-22 12:57:01 +0900

    Require https in release

---
+++ src/RNEmergency/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Responses;
+using Nancy.TinyIoc;
+
+namespace RNEmergency
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+#if RELEASE
+            pipelines.BeforeRequest.AddItemToEndOfPipeline(ctx => ctx.Request.Url.IsSecure ? null : new RedirectResponse("https://" + ctx.Request.Url.HostName + GetPort(ctx.Request.Url.Port)));
+#endif
+        }
+
+        private static string GetPort(int? port)
+        {
+            return (!port.HasValue) ?
+                string.Empty :
+                string.Concat(":", port.Value);
+        }
+    }
+}
