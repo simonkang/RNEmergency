ed89a546164f838caa675289d9bc747e0aa7f9f3
Simon Kang <simon.kang@gmail.com>
2013-04-24 08:21:47 +0900

    Save Sign Image

--- src/RNEmergency/Model/PetitionResult.cs
+++ src/RNEmergency/Model/PetitionResult.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace RNEmergency.Model
 {
     public class PetitionResult
     {
+        private string _sign_image;
+        private string _sign_image_dataurl;
+
         public string email { get; set; }
         public string name { get; set; }
         public string phone_no { get; set; }
         public string daum_id { get; set; }
         public string work_place { get; set; }
         public string client_ip { get; set; }
-        public string sign_image { get; set; }
+        public string sign_image
+        {
+            get { return _sign_image; }
+            set
+            {
+                _sign_image = value;
+                _sign_image_dataurl = "data:image/png;base64," + _sign_image;
+            }
+        }
         public DateTime insert_dt { get; set; }
+
         public string insert_dt_formatted { get { return insert_dt.ToString("yyyy-MM-dd HH:mm:ss"); } }
         public string err_msg { get; set; }
         public bool hasErr { get { return !string.IsNullOrWhiteSpace(err_msg); } }
@@ -29,6 +42,23 @@ namespace RNEmergency.Model
             set
             {
                 sign_image = Convert.ToBase64String(value);
+            }
+        }
+
+        public string sign_image_dataurl
+        {
+            get { return _sign_image_dataurl; }
+            set
+            {   
+                if (value.StartsWith("data", StringComparison.OrdinalIgnoreCase))
+                {
+                    sign_image = value.Substring(value.IndexOf(',') + 1);
+                }
+                else
+                {
+                    sign_image = value;
+                }
+                _sign_image_dataurl = value;
             }
         }
     }
--- src/RNEmergency/Modules/HomeModule.cs
+++ src/RNEmergency/Modules/HomeModule.cs
@@ -29,12 +29,31 @@ namespace RNEmergency.Modules
                         return ex.ToString();
                     }
                 };
-            Post["/"] = p =>
+            Post["/Petition"] = p =>
                 {
                     var pr = this.Bind<PetitionResult>();
                     pr.client_ip = System.Web.HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? Context.Request.UserHostAddress;
                     pr.insert_dt = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, koreaTZI);
-                    pr.err_msg = repo.InsertPetitionResult(pr);
+                    if (string.IsNullOrWhiteSpace(pr.email))
+                    {
+                        pr.err_msg = "email required";
+                    }
+                    else if (string.IsNullOrWhiteSpace(pr.name))
+                    {
+                        pr.err_msg = "name required";
+                    }
+                    else if (string.IsNullOrWhiteSpace(pr.work_place))
+                    {
+                        pr.err_msg = "work place required";
+                    }
+                    else if (string.IsNullOrWhiteSpace(pr.phone_no))
+                    {
+                        pr.err_msg = "phone number required";
+                    }
+                    if (string.IsNullOrWhiteSpace(pr.err_msg))
+                    {
+                        pr.err_msg = repo.InsertPetitionResult(pr);
+                    }
                     return View["result.sshtml", pr];
                 };
         }
